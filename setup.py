1abaa8eaca2463a9d7bbee2a853ce04a5ab9abcb
Ammar Mian <ammar.mian@univ-smb.fr>
2023-05-10 12:52:43 +0200

    added requirements in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['rich', 'rich_click', 'SQLAlchemy',
+                'GitPython', 'pyyaml']
 
 test_requirements = ['pytest>=3', ]
 
